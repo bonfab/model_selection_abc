0b64d5649f6225225efa6863211860b595b26ff9
bonfab <bergmanf@timc-bcm-16.imag.fr>
2019-01-24 18:21:09 +0100

    +

--- GB_vs_NN/generate_data2.R
+++ GB_vs_NN/generate_data2.R
@@ -7,7 +7,7 @@ library(LaplacesDemon)
 library(elasticnet)
 
 
-F_layer <- function(K, F_values, number_locus = 10000, scaling = NULL){
+F_layer <- function(K, F_values, number_locus = 10000){
 
     ancestral_pop <- runif(number_locus)
 
@@ -30,19 +30,34 @@ F_layer <- function(K, F_values, number_locus = 10000, scaling = NULL){
     return(F)
 }
 
-admixture_layer <- function(K, pop_size, number_admixed){
+admixture_layer <- function(K, pop_size, number_admixed, scaling = NULL){
 
-    Q <- matrix(0, nrow = pop_size* (K + number_admixed), ncol = K)
+    if(is.null(scaling)){
+        sizes <- rep(pop_size, K)
+    } else {
+        sizes <- sapply(scaling, function(x) ceiling(x*pop_size))
+    }
+
+
+    Q <- matrix(0, nrow = sum(sizes), ncol = K)
 
     for(pop in 1:K){
-        Q[(pop_size*(pop-1) + 1):(pop_size*pop), pop] <- 1
+        if(pop == 1){
+            Q[1:sum(sizes[1:pop]), pop] <- 1
+        } else {
+            Q[(sum(sizes[1:(pop-1)]) +1):sum(sizes[1:pop]),] <- 1
+        }
+
     }
+
+    print(sizes)
 
     for(admixed in (K+1):(K+number_admixed)){
 
         admixed_prior <- rdirichlet(1, rep(1, K))
+        print((sum(sizes[1:(admixed-1)])+1):sum(sizes[1:admixed]))
 
-        Q[(pop_size*(admixed-1) + 1):(pop_size*admixed),] <- t(replicate(pop_size, rdirichlet(1, admixed_prior), simplify = "matrix"))
+        Q[(sum(sizes[1:(admixed-1)])+1):sum(sizes[1:admixed]),] <- t(replicate(pop_size, rdirichlet(1, admixed_prior), simplify = "matrix"))
 
     }
 
@@ -54,7 +69,17 @@ matrix_binom <- function(matrix, ploidy = 1){
 
     for(i in 1:nrow(matrix)){
         for(j in 1:ncol(matrix)){
-            matrix[i,j] <- rbinom(1, ploidy, matrix[i, j])
+            x <- rbinom(1, ploidy, matrix[i, j])
+            if(is.na(x)){
+                #print(matrix[i,j])
+                #print(matrix[i,j] > 1)
+                #print(matrix[i, j] - 1)
+                #print(rbinom(1,1, matrix[i,j]))
+                #print("")
+                x <- 1
+            }
+
+            matrix[i,j] <- x
         }
     }
 
@@ -66,15 +91,9 @@ matrix_binom <- function(matrix, ploidy = 1){
 PCA_summary <- function(data, reduce_to = 25){
 
     data <- data[, apply(data, 2, function(x) !(length(unique(x)) == 1))]
-    for(i in 1:ncol(data)){
-
-        if(length(unique(data[,i])) == 1){
-            print("impossible !!!!!!!!!!!!!!!!")
-        }
-
-    }
 
     pca <- prcomp(data, scale = T)
+
 
     #trunc <- pca$rotation[,1:reduce_to] %*% pca$x[1:reduce_to,1:reduce_to]
 
@@ -87,21 +106,14 @@ PCA_summary <- function(data, reduce_to = 25){
 
 
 Sys.setlocale("LC_MESSAGES", "en_US.utf8")
-F <- F_layer(3, runif(3, 0, 1), number_locus = 10000)
+K <- 5
+F <- F_layer(K, runif(K, 0, 1), number_locus = 10000)
 Q <- admixture_layer(nrow(F), 50, 1)
 
 
-print(F)
-print("")
-print(Q)
-print("")
-
 prob <- Q %*% F
-print(prob)
-print("")
+
 d <- matrix_binom(prob)
 
-
-print(d)
 
 PCA_summary(d)
