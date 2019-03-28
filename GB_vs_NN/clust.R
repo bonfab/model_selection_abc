0922f029981a1ea6f02efcae66e8fa358cd60fee
bonfab <bergmanf@timc-bcm-16.imag.fr>
2019-03-28 16:33:33 +0100

    clust

---
+++ GB_vs_NN/clust.R
@@ -0,0 +1,34 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: bergmanf
+# Created on: 28/03/19
+
+library(mclust)
+
+load_data <- function(RDS_file = "data_K/full_data_2-16.rds"){
+
+  data <- readRDS(RDS_file)
+
+}
+
+
+
+test <- function(data, true_k){
+
+    print("true_k:")
+    print(true_k)
+    print("estimate:")
+    print(Mclust(data, 1:20, c("EII"))$G)
+
+}
+
+d <- load_data()
+
+for(i in 1:length(d[[1]])){
+    test(d[[1]][[i]], d[[2]][[i]])
+}
+
+#for(d in load_data()){
+ #   print(d[[2]])
+#    test(d[[1]], d[[2]])
+#}
