880a753742d10cd3f0752e6c02e27333e4967eea
Fabian Bergmann <ftbergmann@web.de>
2019-04-03 23:16:14 +0200

    +

--- GB_vs_NN/clust.R
+++ GB_vs_NN/clust.R
@@ -4,6 +4,7 @@
 # Created on: 28/03/19
 
 library(mclust)
+library(RSpectra)
 
 load_data <- function(RDS_file = "data_K/full_admixed1_data_pop_2-16.rds"){
 
@@ -11,6 +12,15 @@ load_data <- function(RDS_file = "data_K/full_admixed1_data_pop_2-16.rds"){
 
 }
 
+reduce_dim <- function(data, reduce_to = 25){
+  eigval <- svds(data, k = reduce_to, nu = reduce_to, nv = 0)
+  U <- eigval$A$u
+  d <- eigval$A$d
+  d <- diag(d)
+  
+  data %*% U %*% solve(d)
+  
+}
 
 
 test <- function(data, true_k){
@@ -25,7 +35,7 @@ test <- function(data, true_k){
 d <- load_data()
 
 for(i in 1:length(d[[1]])){
-    test(d[[1]][[i]], d[[2]][[i]])
+    test(reduce_dim(d[[1]][[i]]), d[[2]][[i]])
 }
 
 #for(d in load_data()){
